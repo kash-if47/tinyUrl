f975ff9dba44afc2f0dfc9c66d4eb022bfa98b33
Kashif Iqbal <kashif.iqbal@aa.com>
2020-06-08 16:51:30 -0500

    added tracking and get url functionality

--- src/tinyUrl.py
+++ src/tinyUrl.py
@@ -16,6 +16,8 @@ from pip._vendor.distlib.compat import raw_input
 
 class TinyUrlConverter:
     store = {}
+    reverseStore = {}
+    storeTrack = {}
     selectChar = "qwertyuiopasdfghjklzxcvbnm1234567890"
     domain = "http://localhost/"
 
@@ -24,15 +26,29 @@ class TinyUrlConverter:
         result = ""
         for i in range(length):
             result = result + self.selectChar[random.randint(0, len(self.selectChar) - 1)]
-        print("Random: ", result)
+        # print("Random: ", result)
         return result
 
     #Converts a long URL to tinyurl
     def generateUrl (self, longUrl):
-        key = self.randomKey(5)
-        self.store[key] = longUrl
+        if(self.reverseStore.get(longUrl) == None):
+            key = self.randomKey(5)
+            self.store[key] = [longUrl, 0]
+            self.reverseStore[longUrl] = key
+        else:
+            return self.domain + self.reverseStore[longUrl]
         return self.domain + key
 
     #Converts a short URL into a Long URL
+    def getLongUrl (self, key):
+        if(self.store.get(key) != None):
+            self.store.get(key)[1] = self.store.get(key)[1] + 1
+            return self.store.get(key)[0]
+        return -1
 
+    def getTracking(self, key):
+        if (self.store.get(key) != None):
+            return self.store.get(key)[1]
+        else:
+            -1
 
--- test/tinyUrlTest.py
+++ test/tinyUrlTest.py
@@ -13,11 +13,31 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(result.startswith(self.domain), True)
         result = result.replace(self.domain, '')
         self.assertEqual(bool(re.match('^[a-z0-9]+$', result)), True)
+        return result
 
     def test_randomGenerator(self):
         result = self.tinyUrl.randomKey(5)
-        print(result)
+        # print(result)
         self.assertEqual(bool(re.match('^[a-z0-9]+$', result)), True)
+
+    def test_getLongUrl(self):
+        result = self.test_generateTinyUrl()
+        longUrl = self.tinyUrl.getLongUrl(result)
+        self.assertEqual("https://www.t-mobile.com/cell-phone/samsung-galaxy-note10-plus-5g?sku=610214662927",longUrl)
+
+    def test_duplicateUrl(self):
+        result = self.test_generateTinyUrl()
+        result2 = self.test_generateTinyUrl()
+        self.assertEqual(result, result2)
+
+    def test_tracker(self):
+        key = self.test_generateTinyUrl()
+        self.test_getLongUrl()
+        self.test_getLongUrl()
+        self.test_getLongUrl()
+        tracking = self.tinyUrl.getTracking(key)
+        self.assertEqual(tracking, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
